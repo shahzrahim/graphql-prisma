057cf8230994a41efb9e54364f40548f0d8fec73
shazi <shaz.rahim@yahoo.com>
2019-07-19 18:07:19 -0500

    final prisma binding using mutations and querys support

--- prisma/src/prisma.js
+++ prisma/src/prisma.js
@@ -8,9 +8,49 @@ const prisma = new Prisma({
 // prisma.query prisma.mutation prisma.subscription prisma.exists
 
 prisma.query.users(null, '{ id name posts { id title } }').then((data) => {
-    console.log(JSON.stringify(data, undefined, 2))
+    // console.log(JSON.stringify(data, undefined, 2))
 })
 
 prisma.query.comments(null, '{ id text author { id name } }').then((data) => {
-    console.log(JSON.stringify(data, undefined, 2))
+    // console.log(JSON.stringify(data, undefined, 2))
+})
+
+// prisma.mutation.createPost({
+//   data: {
+//     title: "My next graphQl post",
+//     body: "Body of text 2",
+//     published: true,
+//     author: {
+//         connect: {
+//           id: "cjyabwo0j00ev0625x9q4i8h1"
+//         }
+//     }
+//   }
+// }, '{ id title body published author { id name }}').then((data) => {
+//     console.log(data, "first one")
+//     return prisma.query.users(null, '{ id name posts { id title } }')
+//   }).then((data) => {
+//     console.log(JSON.stringify(data, undefined, 2), 'the second one')
+// })
+
+
+prisma.mutation.updatePost({
+  where: {
+    id: "cjyaparz3019h062533i72gxd"
+  },
+  data: {
+    title: "My updat234234234ed post",
+    body: "Body of text 1234234",
+    published: false,
+    author: {
+        connect: {
+          id: "cjyabwo0j00ev0625x9q4i8h1"
+        }
+    }
+  }
+}, '{ id title body published author { id name }}').then((data) => {
+    console.log(JSON.stringify(data, undefined, 2), "first one")
+    return prisma.query.posts(null, '{ id title body published author { id name } }')
+  }).then((data) => {
+    console.log(JSON.stringify(data, undefined, 2), 'the second one')
 })
